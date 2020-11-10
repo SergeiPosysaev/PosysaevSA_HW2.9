d861d1c2652c264056018eb6d9fe47e24c95c30f
SergeiPosysaev <zilogfps@yandex.ru>
2020-11-10 11:24:37 +0300

    Initial Commit

---
+++ PosysaevSA_HW2.9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PosysaevSA_HW2.9
+//
+//  Created by Sergei Posysaev on 10.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
